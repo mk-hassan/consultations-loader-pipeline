91bb60fdb4e25f24a5fcfc8add383dbf2f36dfe8
Mahmoud kamal <42426976+mk-hassan@users.noreply.github.com>
2024-10-26 22:21:51 +0000

    chor: refactoring email block creation, making creation safer

--- blocks/email_block.py
+++ blocks/email_block.py
@@ -2,8 +2,11 @@ import os
 
 from prefect_email import EmailServerCredentials
 
-credentials = EmailServerCredentials(
-    username=os.getenv("ADMIN_EMAIL"),
-    password=os.getenv("ADMIN_PASSCODE"),
-)
-credentials.save(name="email-notify")
+try:
+    EmailServerCredentials.load("email-notify")
+except ValueError:
+    credentials = EmailServerCredentials(
+        username=os.getenv("ADMIN_EMAIL"),
+        password=os.getenv("ADMIN_PASSCODE"),
+    )
+    credentials.save(name="email-notify")
